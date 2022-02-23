c3db27e10aa6f8fa16907a2eeb6334b2578565f9
Lian206 <2236370700@qq.com>
2022-02-23 14:38:01 +0800

    init

--- common/config.js
+++ common/config.js
@@ -5,8 +5,7 @@ if (process.env.NODE_ENV === 'development'){
     url_config = "http://127.0.0.1:9998"
 } else {
     // 生产环境
-    // url_config  = 'http://82.157.165.193:9998'
-	url_config = "https://www.lianlian.store"
+	url_config = "https://xxxxxx"
 }
  
 export default url_config
